3e38dce2b7dc13db34855b714ecbb74a557c3355
irina <irina@me.com>
2019-06-17 13:02:24 +0300

    comment

--- src/main/java/com/mytests/spring/thymeleaf/thymeleaf_verification1/ThymeleafVerification1Application.java
+++ src/main/java/com/mytests/spring/thymeleaf/thymeleaf_verification1/ThymeleafVerification1Application.java
@@ -3,6 +3,9 @@ package com.mytests.spring.thymeleaf.thymeleaf_verification1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ */
 @SpringBootApplication
 public class ThymeleafVerification1Application {
 
